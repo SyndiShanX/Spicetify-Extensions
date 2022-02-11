77f239a2e718977a2590d21ec9ab106b311c75b4
SyndiShanX <brianmatthew.1026@gmail.com>
2022-02-11 13:46:07 -0500

    Update fixTippyBoxes.js

--- fixTippyBoxes/fixTippyBoxes.js
+++ fixTippyBoxes/fixTippyBoxes.js
@@ -8,13 +8,19 @@
 	
 	async function fixTippyBoxes() {
 		if (document.getElementsByClassName("X8yW2lJbFCQfV5GjoRwL")[0] != undefined) {
-			console.log('Exists')
-			setTimeout(function(){
-				document.getElementsByClassName("X8yW2lJbFCQfV5GjoRwL")[0].parentElement.parentElement.id = 'tippy-test';
-			}, 50);
-			setTimeout(function(){ fixTippyBoxes() }, 50);
+			if (document.getElementsByClassName("X8yW2lJbFCQfV5GjoRwL.parentElement")[0] != undefined) {
+				if (document.getElementsByClassName("X8yW2lJbFCQfV5GjoRwL.parentElement.parentElement")[0] != undefined) {
+					setTimeout(function(){
+						document.getElementsByClassName("X8yW2lJbFCQfV5GjoRwL")[0].parentElement.parentElement.id = 'tippy-test';
+					}, 50);
+					setTimeout(function(){ fixTippyBoxes() }, 50);
+				} else {
+					setTimeout(function(){ fixTippyBoxes() }, 50);
+				}
+			} else {
+				setTimeout(function(){ fixTippyBoxes() }, 50);
+			}
 		} else {
-			console.log('Running Test')
 			setTimeout(function(){ fixTippyBoxes() }, 50);
 		}
 	}
